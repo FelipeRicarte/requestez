701305765f84a1281dc590d3d206ab41cd99103c
Felipe Ricarte <felipericartem@gmail.com>
2018-02-04 21:55:27 -0300

    Criação do resource de categorias.

--- src/main/java/com/union/requestez/resource/CategoriaResource.java
+++ src/main/java/com/union/requestez/resource/CategoriaResource.java
@@ -1,4 +1,17 @@
 package com.union.requestez.resource;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar(){
+        return "testando essa porra";
+    }
+
 }
