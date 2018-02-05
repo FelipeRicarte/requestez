ef7a49642628fdda69d2a95ba7198073487692a9
Felipe Ricarte <felipericartem@gmail.com>
2018-02-04 21:41:13 -0300

    Criação da estrutura inicial

---
+++ src/main/java/com/union/requestez/RequestezApplication.java
@@ -0,0 +1,12 @@
+package com.union.requestez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestezApplication.class, args);
+	}
+}
